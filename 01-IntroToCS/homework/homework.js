97faedc9efa7423d227f562e9699d826f13c7852
JohnCrossRollOn <juanrolon54@outlook.com>
2022-08-16 17:25:48 -0300

    intro to cs homework done

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,28 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  //return [...num].reverse().map((e, i)=>{return e==='1'?2**i:0}).reduce((t, v)=> t+v)
+  return [...num].reverse().map((e, i)=>{return e==='1'?2**i:0}).reduce((t, v)=> t+v)
 
-  return parseInt(num, 2)
+  //return parseInt(num, 2) //lazy way
 }
 
 function DecimalABinario(num) {
       // tu codigo aca
 
-	// let a = [num];
-	// do {
-	// if (a[a.length-1]%2===0) {
-	// a[a.length-1] /= 2;
-	// a = ['0' ,...a];
-	// } else {
-	// a[a.length-1] /= 2;
-	// a[a.length-1] = Math.floor(a[a.length-1])
-	// a = ['1' ,...a];
-	// }
-	// } while (a[a.length-1] >= 1)
-	// return a.splice(0,a.length-1).join('')
+	let a = [num];
+	do {
+	if (a[a.length-1]%2===0) {
+	a[a.length-1] /= 2;
+	a = ['0' ,...a];
+	} else {
+	a[a.length-1] /= 2;
+	a[a.length-1] = Math.floor(a[a.length-1])
+	a = ['1' ,...a];
+	}
+	} while (a[a.length-1] >= 1)
+	return a.splice(0,a.length-1).join('')
 
-  return num.toString(2)
+  //return num.toString(2) //yes, i'm indeed that lazy
 }
 
 
