f02827ba39c41f538c9c13389683b4bc6145b49a
JohnCrossRollOn <juanrolon54@outlook.com>
2022-08-23 19:44:25 -0300

    why are we still here? just to suffer. Also, learned about binary search trees and how to iterate through them.

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,4 +1,4 @@
-"use strict";
+;"use strict";
 
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
@@ -11,7 +11,61 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+
+function BinarySearchTree(value) {
+	this.n = 1;
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function() {
+  return this.n;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  this.n++;
+  if (value < this.value) {return !this.left? this.left = new BinarySearchTree(value) : this.left.insert(value)};
+  if (value > this.value) {return !this.right? this.right = new BinarySearchTree(value) : this.right.insert(value)};
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (value < this.value) {return !this.left? false : this.left.contains(value);};
+  if (value > this.value) {return !this.right? false : this.right.contains(value)};
+  return true;
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, str) {
+  function inOrd(node) {
+    node.left && inOrd(node.left);
+    cb(node.value);
+    node.right && inOrd(node.right);
+  }
+  function preOrd(node) {
+    cb(node.value);
+    node.left && preOrd(node.left);
+    node.right && preOrd(node.right);
+  }
+  function posOrd(node) {
+    node.left && posOrd(node.left);
+    node.right && posOrd(node.right);
+    cb(node.value);
+  }
+  if (str == 'pre-order') {preOrd(this)};
+  if (str == 'post-order') {posOrd(this)};
+  if (str == 'in-order' || !str) {inOrd(this)};
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb) {
+  var queue = [this];
+  var node = this;
+  while(queue.length > 0){
+      node = queue.shift();
+      cb(node.value);
+      if(node.left) {queue.push(node.left)};
+      if(node.right) {queue.push(node.right)};
+  }
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
